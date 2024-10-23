3384f8a5a7cca93b9b52e92aca7559d65d9b39d3
HenryDissenha <164203544+HenryDissenha@users.noreply.github.com>
2024-10-23 11:38:10 -0300

    Create listas.js

---
+++ listas.js
@@ -0,0 +1,11 @@
+console.log(`Trabalhando com listas`);
+const salvador = `Salvador`;
+const saoPaulo = `São Paulo`;
+const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDeDestinos = new Array(
+    `salvador`
+)
+
+console.log("Destinos possíveis:")
+console.log(salvador, saoPaulo, rioDeJaneiro)
